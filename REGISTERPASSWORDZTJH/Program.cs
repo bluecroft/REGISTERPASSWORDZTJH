6158b7b412f6358f9c3f7b83a15596da10c5a7f0
Thomas Hoque <K31361@eltham-college.org.uk>
2020-02-12 15:06:53 +0000

    Add project files.

---
+++ REGISTERPASSWORDZTJH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace REGISTERPASSWORDZTJH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Username;
+            Console.WriteLine("Write in your username");
+            Console.ReadLine();
+            Username = Console.ReadLine();
+            string Password;
+            Console.WriteLine("Type in your password!!!");
+            Console.ReadLine();
+            Password = Console.ReadLine();
+
+             Register;
+            Register = (Username, Password);
+        }
+    }
+}
